84aff4aece5f9ddd017eda53a549d2a4e3b64171
friction99 <110677112+friction99@users.noreply.github.com>
2023-12-10 17:42:48 +0530

    Swiggy api issue resolved

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import RestCard from "./RestCard";
-import { useState } from "react";
+import { useState,useEffect} from "react";
 import data from "../utils/mockdata";
 const Body = () => {
     const [listOfResturants,setlistOfResturant] = useState(data);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    const fetchData = async ()=>{
+        const fetched = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.618423899727492&lng=85.11005479543813&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await fetched.json();
+        setlistOfResturant(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
+    };
     return (
         <div className="body">
             <div className="Filter">{/*This is a comment*/}
                 <button onClick={()=>{
-                    const filteredData=listOfResturants.filter((res)=>res.avgRating>4);
+                    const filteredData=listOfResturants.filter((res)=>res.info.avgRating>4);
                     setlistOfResturant(filteredData);
                 }}>Top Rated Resturant</button>
             </div>
             <div className="res-cards">
                 {
                     listOfResturants.map((resturant)=> (
-                        <RestCard key = {resturant.id} resData={resturant}/>
+                        <RestCard key = {resturant.info.id} resData={resturant.info}/>
                     ))
                 }
             </div>
--- src/utils/mockdata.js
+++ src/utils/mockdata.js
@@ -1,26 +1,172 @@
-const data = [{
-    id:"711045",
-    name:"Chicago Pizza",
-    cloudinaryImageId:"80f040545552605e33eba09f8fa30be9",
-    locality:"Mourya Lok Complex",
-    areaName:"Fraser Road",
-    costForTwo:"₹500 for two",
-    cuisines:["Fast Food","Pizzas"],
-    avgRating:3,
-    totalFee:2400,
-    parentId:"60277",
-    avgRatingString:"3.0"
-},{
-	id: "654087",
-	name: "Burger King",
-	cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
-	locality: "SP Verma Road",
-	areaName: "Lodipur",
-	costForTwo: "₹350 for two",
-	cuisines: [
-		"Burgers",
-		"American"
-	],
-	avgRating: 4.7
-}];
+const data = [
+    {
+        "info": {
+            "id": "563340",
+            "name": "Chicago Pizza",
+            "cloudinaryImageId": "kkqcux79dpt6h3kd0fcj",
+            "locality": "Anisabad",
+            "areaName": "Anisabad",
+            "costForTwo": "₹400 for two",
+            "cuisines": [
+                "Pizzas"
+            ],
+            "avgRating": 3.7,
+            "feeDetails": {
+                "restaurantId": "563340",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 4700
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 4700
+            },
+            "parentId": "60277",
+            "avgRatingString": "3.7",
+            "totalRatingsString": "500+",
+            "sla": {
+                "deliveryTime": 36,
+                "lastMileTravel": 7.2,
+                "serviceability": "SERVICEABLE",
+                "slaString": "36 mins",
+                "lastMileTravelString": "7.2 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-12-10 23:00:00",
+                "opened": true
+            },
+            "badges": {},
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {},
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "₹125 OFF",
+                "subHeader": "ABOVE ₹199",
+                "discountTag": "FLAT DEAL"
+            },
+            "loyaltyDiscoverPresentationInfo": {
+                "logoCtx": {
+                    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+                },
+                "freedelMessage": "FREE DELIVERY",
+                "badgeType": "BADGE_TYPE_ONE_LITE"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {},
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/chicago-pizza-anisabad-patna-563340",
+            "type": "WEBLINK"
+        }
+    },
+    {
+        "info": {
+            "id": "422805",
+            "name": "KFC",
+            "cloudinaryImageId": "f01666ac73626461d7455d9c24005cd4",
+            "locality": "Radhey Shree Road",
+            "areaName": "Patliputra Colony",
+            "costForTwo": "₹400 for two",
+            "cuisines": [
+                "Burgers",
+                "Biryani",
+                "American",
+                "Snacks",
+                "Fast Food"
+            ],
+            "avgRating": 4.1,
+            "feeDetails": {
+                "restaurantId": "422805",
+                "fees": [
+                    {
+                        "name": "BASE_DISTANCE",
+                        "fee": 2300
+                    },
+                    {
+                        "name": "BASE_TIME"
+                    },
+                    {
+                        "name": "ANCILLARY_SURGE_FEE"
+                    }
+                ],
+                "totalFee": 2300
+            },
+            "parentId": "547",
+            "avgRatingString": "4.1",
+            "totalRatingsString": "1K+",
+            "sla": {
+                "deliveryTime": 25,
+                "lastMileTravel": 2.4,
+                "serviceability": "SERVICEABLE",
+                "slaString": "25 mins",
+                "lastMileTravelString": "2.4 km",
+                "iconType": "ICON_TYPE_EMPTY"
+            },
+            "availability": {
+                "nextCloseTime": "2023-12-10 23:00:00",
+                "opened": true
+            },
+            "badges": {},
+            "isOpen": true,
+            "type": "F",
+            "badgesV2": {
+                "entityBadges": {
+                    "imageBased": {},
+                    "textBased": {},
+                    "textExtendedBadges": {}
+                }
+            },
+            "aggregatedDiscountInfoV3": {
+                "header": "20% OFF",
+                "subHeader": "UPTO ₹50"
+            },
+            "loyaltyDiscoverPresentationInfo": {
+                "logoCtx": {
+                    "logo": "Swiggy%20One%20Lite/One_lite_vertical_logo.png"
+                },
+                "freedelMessage": "FREE DELIVERY",
+                "badgeType": "BADGE_TYPE_ONE_LITE"
+            },
+            "differentiatedUi": {
+                "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+                "differentiatedUiMediaDetails": {
+                    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+                    "lottie": {},
+                    "video": {}
+                }
+            },
+            "reviewsSummary": {},
+            "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+            "restaurantOfferPresentationInfo": {}
+        },
+        "analytics": {},
+        "cta": {
+            "link": "https://www.swiggy.com/restaurants/kfc-radhey-shree-road-patliputra-colony-patna-422805",
+            "type": "WEBLINK"
+        }
+    }
+];
 export default data;
